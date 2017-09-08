dcd951b4431360f24454ac3e2246368bdd6b656e
alexhong742 <alexhong742@users.noreply.github.com>
2017-09-08 12:11:25 -0700

    got rid of html and switched to react for login/signup

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req,res) => {
-  res.sendFile(path.join(__dirname, '../client/login.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 app.get("/signup", (req,res) => {
   res.sendFile(path.join(__dirname, '../client/signup.html'));
@@ -42,14 +42,7 @@ app.post("/login",
   (req,res,next) => {
     console.log(req.body ,"request body from sign up")
     res.redirect('/login')
-    
 });
-
-
-
-
-
-
 
 app.get("/assets/style.css", (req,res) => {
   res.sendFile(path.join(__dirname, '../client/assets/style.css'));
