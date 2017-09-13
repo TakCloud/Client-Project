37ee9a423746783dcf0dcecd3bb181666ca290be
JAPerera <jon.perera@hotmail.com>
2017-09-13 15:37:30 -0700

    Update App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,25 +1,9 @@
 import React from 'react';
-import axios from 'axios';
-import message from './../messages/firstMessage';
 
-class App extends React.Component {
-  state = {
-    email: message,
-  };
-  poster = () => {
-    axios.post('/alex', this.state.email)
-      .then(() => {
-        window.location = '/alex ';
-      });
-  };
+const App = (() => (
+  <div>
+    <h1>Hello</h1>
+  </div>
+));
 
-  render() {
-    return (
-      <div>
-        <h1>Hello</h1>
-        <input type="submit" value="Submit" onClick={this.poster} />
-      </div>
-    );
-  }
-}
 export default App;
