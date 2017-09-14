4c7a3f4f6ea2601978fb8ebac263063c75dcefea
JAPerera <jon.perera@hotmail.com>
2017-09-13 19:11:02 -0700

    Update authUrlProvider.js

--- server/controllers/authUrlProvider.js
+++ server/controllers/authUrlProvider.js
@@ -1,10 +1,8 @@
 const oauth2Client = require('./oauthCreator').oauth2Client;
 const url = require('./oauthCreator').url;
-const token = require('../access_token.json');
 
 module.exports = (req, res, next) => {
   console.log('This token was passed to the front end: ', token);
   res.locals.authUrl = url;
-  oauth2Client.credentials = token;
   next();
 };
