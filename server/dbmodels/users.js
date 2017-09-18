bb09b1d9af28b088c900ee34003a9bdfd52bcf42
Jon Perera <jon.perera@hotmail.com>
2017-09-17 17:45:48 -0700

    added initial db models for sequelize

---
+++ server/dbmodels/users.js
@@ -0,0 +1,78 @@
+const Sequelize = require('sequelize');
+const sequelize = require('./../dbcontrollers/sequelize_instance.js');
+
+const Users = sequelize.define('users', {
+  user_id: {
+    type: Sequelize.INTEGER,
+    unique: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  user_organization_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'user_organizations',
+      key: 'organization_id',
+    },
+  },
+  user_organization_name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    references: {
+      model: 'user_organizations',
+      key: 'organization_name',
+    },
+  },
+  user_first_name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  user_last_name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  user_email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  user_password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  company_name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email_signature: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  send_as_email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  reply_to_email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  gmail_access_token: {
+    type: Sequelize.STRING,
+  },
+  gmail_refresh_token: {
+    type: Sequelize.STRING,
+  },
+}, {
+  freezeTableName: true,
+  tableName: 'users',
+  timestamps: false,
+});
+
+module.exports = () => Users;
