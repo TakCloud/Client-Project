938659464eb7499b2650dc98274ee6665e337dfb
Jon Perera <jon.perera@hotmail.com>
2017-09-27 20:08:20 -0700

    refactored engine to only make one query to the database and incorporated new refresh token logic from Alex

--- server/engine/sendEmail.js
+++ server/engine/sendEmail.js
@@ -4,6 +4,7 @@ module.exports = (email, mailer, message) => {
   // console.log('look at this: ', message);
   mailer.sendMail(message)
     .then(() => {
+      console.log(`Email sent to: ${email.lead.lead_email}`);
       // after sending, the email record needs to be updated in the database
       // so the engine doesn't pull it again
       updateEmailRecord(email, message);
