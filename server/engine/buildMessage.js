612ec6df661a10e2be72b8df59719eba5b093819
alexhong742 <alexhong742@users.noreply.github.com>
2017-10-07 17:44:23 -0700

    engine

--- server/engine/buildMessage.js
+++ server/engine/buildMessage.js
@@ -1,35 +1,27 @@
 const models = require('./../dbmodels/dbmodels.js');
-const verifyTransporter = require('./verifyTransporter.js');
+// const verifyTransporter = require('./verifyTransporter.js');
+const sendMail = require('./sendEmail.js');
 
-module.exports = (emailSet, mailer, user) => {
+module.exports = (emailSet, mailer) => {
   // run loop to build each email and send
-  emailSet.forEach((email) => {
-    models.leads.find({
-      where: { lead_id: email.lead_id },
-      attributes: ['lead_id', 'lead_first_name', 'lead_last_name', 'lead_email'],
-    })
-      .then((leadRecord) => {
-        const lead = leadRecord.dataValues;
-        const message = {
-          envelope: {
-            from: user.user_email,
-            to: lead.lead_email,
-            dsn: {
-              id: 'some random message specific id',
-              return: 'headers',
-              notify: ['failure', 'delay', 'success'],
-              recipient: user.user_email,
-            },
-          },
-          subject: email.subject,
-          html: `<b><img src="https://cheatcodes5.herokuapp.com/summary/imageTracker?thisPersonOpenedEmail=${lead.lead_email}"/></b>`,
-          text: email.body,
-        };
-        // verify that users access token is valid before sending
-        verifyTransporter(email, mailer, message, user, lead);
-      })
-      .catch((err) => {
-        console.log(`Something went wrong when grabbing lead: ${err}`);
-      });
+  emailSet[1].emails.forEach((email) => {
+    const message = {
+      envelope: {
+        from: emailSet[1].user_email,
+        to: email.lead.lead_email,
+        dsn: {
+          id: 'some random message specific id',
+          return: 'headers',
+          notify: ['failure', 'delay', 'success'],
+          recipient: emailSet[1].user_email,
+        },
+      },
+      subject: email.subject,
+      html: `<b><img src="https://cheatcodes5.herokuapp.com/summary/imageTracker?thisPersonOpenedEmail=${email.lead.lead_email}"/></b>`,
+      text: email.body,
+    };
+    // send each email after being built
+    sendMail(email, mailer, message);
   });
 };
+
