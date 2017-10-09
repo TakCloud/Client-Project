7429210af4978a57a05de5f08336c14bd738d634
alexhong742 <alexhong742@users.noreply.github.com>
2017-10-09 11:17:49 -0700

    add engine

--- server/server.js
+++ server/server.js
@@ -111,5 +111,5 @@ app.post('/summary',
 app.listen(port, () => {
   console.log(`now listening on ${port}! \n`);
   // ** UNCOMMENT TO START ENGINE **  
-  // engine.begin();
+  engine.begin();
 });
