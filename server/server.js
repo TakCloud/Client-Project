71122c2069a85bc4af6a4b0a677baec3b6895fba
alexhong742 <alexhong742@users.noreply.github.com>
2017-10-09 12:08:54 -0700

    added /imageTracker

--- server/server.js
+++ server/server.js
@@ -33,9 +33,9 @@ app.get('/build/bundle.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/bundle.js'));
 });
 
-app.get('/summary/imageTracker?', (req, res) => {
+app.get('/summary/imageTracker', (req, res) => {
   console.log('Ping*IMAGE TRACKER WAS HIT!!!!*ponG \n', req.query);
-  res.sendFile(path.join(__dirname, '../4-tree-png-image-download-picture.png'));
+  res.sendFile(path.join(__dirname, '../client/4-tree-png-image-download-picture.png'));
 });
 
 
