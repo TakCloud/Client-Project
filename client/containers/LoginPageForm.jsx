ad03eb71dfaf17f8c89523c9a74f3743366bee96
JTak <jasonytak@gmail.com>
2017-10-11 10:57:00 -0700

    Fixing Front End

--- client/containers/LoginPageForm.jsx
+++ client/containers/LoginPageForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { TextField } from 'redux-form-material-ui';
 import RaisedButton from 'material-ui/RaisedButton';
--- client/containers/NewCampaignFormConfirmationPage.jsx
+++ client/containers/NewCampaignFormConfirmationPage.jsx
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { postNewCampaign } from '../actions/postNewCampaign';
 
 class NewCampaignFormConfirmationPage extends Component {
