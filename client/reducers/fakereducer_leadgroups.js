0bc6a55dd5e9902dfd91d7e774a5a20fdf436e96
JTak <jasonytak@gmail.com>
2017-10-13 16:12:23 -0700

    Front End CSS updates

--- client/reducers/fakereducer_leadgroups.js
+++ client/reducers/fakereducer_leadgroups.js
@@ -2,17 +2,17 @@ export default function () {
   return [
     {
       leadgroup_id: 1,
-      leadgroup_name: 'VIP',
+      leadgroup_name: 'VIP Clients',
       leadgroup_emails: ['something@gmail.com', 'hello@codesmith.io'],
     },
     {
       leadgroup_id: 2,
-      leadgroup_name: 'Codesmith',
+      leadgroup_name: 'World Freight Forwarders',
       leadgroup_emails: ['tak@hotmail.com', 'goodbye@gmail.com'],
     },
     {
       leadgroup_id: 3,
-      leadgroup_name: 'COHORT',
+      leadgroup_name: 'Commodity Forwarders Inc.',
       leadgroup_emails: ['tak@hotmail.com', 'goodbye@gmail.com'],
     },
   ];
