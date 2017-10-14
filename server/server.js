97cb15ffe2de84f3d712477f1e97cb80e67aa63a
Alex Hong <alexhong742@gmail.com>
2017-10-14 10:29:48 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -116,5 +116,5 @@ app.post('/summary',
 app.listen(port, () => {
   console.log(`now listening on ${port}! \n`);
   // ** UNCOMMENT TO START ENGINE **  
-  // engine.begin();
+  engine.begin();
 });
