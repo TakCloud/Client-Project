7a316f4f433a31b82c617b849096b65395a2322b
alexhong742 <alexhong742@users.noreply.github.com>
2017-10-17 14:25:59 -0700

    azure deploy demo

--- client/containers/NewCampaignFormSummaryPage.jsx
+++ client/containers/NewCampaignFormSummaryPage.jsx
@@ -9,7 +9,7 @@ import ActionDelete from 'material-ui/svg-icons/action/delete';
 
 const NewCampaignFormSummaryPage = () => {
   return (
-    <div className="newcampaign-container">
+    <div className="newcampaign-last-page-container">
       <AppBar
         title="Campaign Summary"
         className="first-page-header"
@@ -17,28 +17,32 @@ const NewCampaignFormSummaryPage = () => {
         style={{ height: '100px', backgroundColor: '#2196F3' }}
       />
       <div className="steps-container">
-        <h3 className="first-page-title">Campaign Name: New Leads</h3>
-        <h3 className="first-page-title">Recipient Group: USA Freight</h3>
+        <h3 className="first-page-title">Campaign Name: New Asia Route (10/25)</h3>
+        <h3 className="first-page-title">Recipient Group: Asia Leads</h3>
         <Toolbar className="campaign-summary-toolbar">
           <ToolbarGroup>
-            <ToolbarTitle className="campaign-summary-toolbar-title" text="Step 1 - We have a deal for you!" />
+            <ToolbarTitle className="campaign-summary-toolbar-title" text="Step 1 - New Asia Opportunity" />
             <ToolbarSeparator />
-            <EditorModeEdit />
-            <ActionDelete />
+            <div className="campaign-summary-step-buttons">
+              <EditorModeEdit />
+              <ActionDelete />
+            </div>
           </ToolbarGroup>
         </Toolbar>
       </div>
-      <RaisedButton
-        label="Add Step"
-        className="add-new-email"
-        primary
-      />
-      <RaisedButton
-        label="Confirm Campaign"
-        className="campaign-form-next-buttons"
-        secondary
-        containerElement={<Link to={'/summary'} />}
-      />
+      <div className="campaign-summary-buttons-container">
+        <RaisedButton
+          label="Add Step"
+          className="add-new-email"
+          primary
+        />
+        <RaisedButton
+          label="Confirm"
+          className="campaign-form-next-buttons"
+          secondary
+          containerElement={<Link to={'/summary'} />}
+        />
+      </div>
     </div>
   );
 };
